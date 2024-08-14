8325f75aca5dff11c3581cca83d6238d99444d7b
yuto <yuto.mmj@gmail.com>
2024-08-14 13:08:08 +0300

    feat: Add a Node class to create a linked list. Add and implement new methods and modify old ones.

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -2,23 +2,95 @@ package managers;
 import tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> history = new ArrayList<>();
+
+    private static class Node {
+        Task task;
+        Node prev;
+        Node next;
+
+        private Node(Task task, Node prev, Node next) {
+            this.task = task;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            String prevTask = (prev != null) ? prev.task.toString() : "null";
+            String nextTask = (next != null) ? next.task.toString() : "null";
+            return task.toString() + " prev: " + prevTask + " next: " + nextTask;
+        }
+    }
+
+    private final Map<Integer, Node> nodeMap = new HashMap<>();
+    private Node first;
+    private Node last;
+
+
+    private void linkLast(Task task) {
+        Node node = new Node(task, last, null);
+        if (last != null) {
+            last.next = node;
+        } else {
+            first = node;
+        }
+        last = node;
+    }
+
+    private List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Node node = first;
+        while (node != null) {
+            tasks.add(node.task);
+            node = node.next;
+        }
+        return tasks;
+    }
+
+    public void removeNode(Node node) {
+        if (node.prev == null) {
+            first = node.next;
+            if (first != null) {
+                first.prev = null;
+            } else {
+                last = null;
+            }
+        } else if (node.next != null) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        } else {
+            last = node.prev;
+            last.next = null;
+        }
+    }
 
     @Override
     public void add(Task task) {
         if (task != null) {
-            if (history.size() == 10) {
-                history.removeFirst();
+            if (nodeMap.containsKey(task.getId())) {
+                remove(task.getId());
             }
-            history.add(task);
+            linkLast(task);
+            nodeMap.put(task.getId(), last);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        return getTasks();
+    }
+
+    @Override
+    public void remove(int id) {
+        Node node = nodeMap.get(id);
+        if (node != null) {
+            removeNode(node);
+            nodeMap.remove(id);
+        }
     }
 }
