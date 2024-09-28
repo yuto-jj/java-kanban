c546d5c26f7f9c31dba68b3f7df544e4a8ce1a7e
yuto <yuto.mmj@gmail.com>
2024-09-28 12:55:42 +0300

    feat: sprint_9-solution-http-api

--- src/server/adapters/LocalDateTimeAdapter.java
+++ src/server/adapters/LocalDateTimeAdapter.java
@@ -2,6 +2,7 @@ package server.adapters;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -13,12 +14,21 @@ public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd.MM.yyyy");
 
     @Override
-    public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
-        jsonWriter.value(localDateTime.format(formatter));
+    public void write(JsonWriter out, LocalDateTime localDateTime) throws IOException {
+        if (localDateTime == null) {
+            out.nullValue();
+        } else {
+            out.value(localDateTime.format(formatter));
+        }
     }
 
     @Override
-    public LocalDateTime read(JsonReader jsonReader) throws IOException {
-        return LocalDateTime.parse(jsonReader.nextString(), formatter);
+    public LocalDateTime read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();  // Пропустить значение null
+            return null;
+        }
+        String dateTimeString = in.nextString();
+        return LocalDateTime.parse(dateTimeString, formatter);
     }
 }
